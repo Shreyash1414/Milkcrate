7b1128b2710a9f417e5d5f8a9dd97130a56d5d0f
Sahil <sahil.dixit15.sd@gmail.com>
2022-06-12 12:21:12 +0530

    formatted code

--- index.js
+++ index.js
@@ -1,59 +1,56 @@
 import express from "express";
 // All Imports
-import { dairyRegestration } from './routes/regestration/dairyRegestration.js';
-import{dairyLogin} from './routes/login/dairyLogin.js';
-import{dairyInfo} from './routes/credentials/dairyInfo.js';
-import{profileInfo} from './routes/credentials/profileInfo.js';
-import{farmerReg} from './routes/regestration/farmerRegistration.js';
+import { dairyRegestration } from "./routes/regestration/dairyRegestration.js";
+import { dairyLogin } from "./routes/login/dairyLogin.js";
+import { dairyInfo } from "./routes/credentials/dairyInfo.js";
+import { profileInfo } from "./routes/credentials/profileInfo.js";
+import { farmerReg } from "./routes/regestration/farmerRegistration.js";
 import { getNumberInfo } from "./routes/credentials/numberInfo.js";
 import { FarmerProfile } from "./routes/credentials/farmerProfile.js";
-import {newMilkEntry} from './routes/Famer/milkEntry.js';
-import {dateInfo} from './routes/credentials/farmerCalander.js';
-import {dairyDateInfo} from './routes/Famer/dairyDateInfo.js';
-import {dairyFarmerTransaction} from './routes/dairyTransaction/dairyTransaction.js';
-import {farmerTransactionHistory} from './routes/transactionFarmer/transactionHistory.js';
+import { newMilkEntry } from "./routes/Famer/milkEntry.js";
+import { dateInfo } from "./routes/credentials/farmerCalander.js";
+import { dairyDateInfo } from "./routes/Famer/dairyDateInfo.js";
+import { dairyFarmerTransaction } from "./routes/dairyTransaction/dairyTransaction.js";
+import { farmerTransactionHistory } from "./routes/transactionFarmer/transactionHistory.js";
 // const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
 
-const port =process.env.PORT || 3000
-app.listen(port , ()=>{
-    console.log("listning at port 3000........");
-})
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("listning at port 3000........");
+});
 
 // api
 // Regestration Api's
-app.post("/api/registration/dairy",dairyRegestration);
+app.post("/api/registration/dairy", dairyRegestration);
 
-
-app.post("/api/login/dairy",dairyLogin);
-
+app.post("/api/login/dairy", dairyLogin);
 
 // diary Credential
-app.post("/api/credentials/dairyId",dairyInfo);
-app.post("/api/dairyProfile/dairyId",profileInfo);
-app.post("/api/numberInfo",getNumberInfo);
-app.post("/api/farmerProfile/dairyId/farmerId",FarmerProfile);
-
+app.post("/api/credentials/dairyId", dairyInfo);
+app.post("/api/dairyProfile/dairyId", profileInfo);
+app.post("/api/numberInfo", getNumberInfo);
+app.post("/api/farmerProfile/dairyId/farmerId", FarmerProfile);
 
 //Farmer Registration
 
-app.post("/api/user/reg",farmerReg);
+app.post("/api/user/reg", farmerReg);
 
 //New Entry
 
-app.post("/api/farmers/newEntry",newMilkEntry);
+app.post("/api/farmers/newEntry", newMilkEntry);
 
 //farmer calendar
-app.post("/api/getFarmerInfo/date",dateInfo);
+app.post("/api/getFarmerInfo/date", dateInfo);
 
 //Dairy Date info
-app.post("/api/getDairyInfo/date",dairyDateInfo);
+app.post("/api/getDairyInfo/date", dairyDateInfo);
 
 //dairy transaction
-app.post("/api/getFarmers/transaction",dairyFarmerTransaction);
+app.post("/api/getFarmers/transaction", dairyFarmerTransaction);
 
 //dairy transaction farmer
-app.post("/api/transaction/transactionFarmer",farmerTransactionHistory);
+app.post("/api/transaction/transactionFarmer", farmerTransactionHistory);
